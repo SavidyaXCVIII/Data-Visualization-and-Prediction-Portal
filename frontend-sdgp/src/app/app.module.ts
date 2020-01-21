f606fa2c02ff082a2d1ada05cbe1a78b659c7ae7
Savidya Udesh <45062811+SavidyaXCVIII@users.noreply.github.com>
2020-01-22 00:47:36 +0530

    + Adding @Angular Material + Setting up the sample home page

--- frontend-sdgp/src/app/app.module.ts
+++ frontend-sdgp/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MatSidenavModule,
+        BrowserAnimationsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
