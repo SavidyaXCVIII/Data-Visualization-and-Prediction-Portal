880af081a0313a09e72a3ce4b2b9acbd2e46c6df
Nadin Kodithuwakku (NK) <nadin.2018430@iit.ac.lk>
2020-02-23 19:28:58 +0530

    Post api#2

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    mongoose = require('mongoose'),
+    config = require('./DB');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => {console.log('Database is connected') },
+    err => { console.log('Can not connect to the database'+ err)}
+);
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, function(){
+    console.log('Listening on port ' + port);
+});
