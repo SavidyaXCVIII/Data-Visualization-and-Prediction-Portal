f660c2a0ea1787dd159865d3f28ae2db96bf0158
Savidya Udesh <45062811+SavidyaXCVIII@users.noreply.github.com>
2020-02-23 21:47:15 +0530

    spring init

---
+++ backend-spring/src/main/java/com/sdgp/backend/BackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sdgp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringApplication.class, args);
+    }
+
+}
