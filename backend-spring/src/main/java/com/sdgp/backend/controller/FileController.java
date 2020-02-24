c2348ff637551a1a2716786e749ccf459e4af137
Savidya Udesh <45062811+SavidyaXCVIII@users.noreply.github.com>
2020-02-24 21:11:20 +0530

    added a test get api for a dataset

--- backend-spring/src/main/java/com/sdgp/backend/controller/FileController.java
+++ backend-spring/src/main/java/com/sdgp/backend/controller/FileController.java
@@ -5,29 +5,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 @RestController
-@RequestMapping("files")
 @CrossOrigin(origins = "*")
 public class FileController {
 
-	@PostMapping("/")
-	ResponseEntity<ResponseDTO> saveFile(@RequestParam("file") MultipartFile dataFile)
-	{
+	public static byte[] bytes;
 
-		try
-		{
-			InputStream inputStream = dataFile.getInputStream();
-			int data = inputStream.read();
-
-		}
-		catch ( IOException e )
-		{
-			e.printStackTrace();
-		}
-
-		return ResponseEntity.ok( new ResponseDTO<>( Boolean.FALSE, "Name : " + dataFile.getName() + ",Size :" + dataFile.getSize() + ", ContentType : " + dataFile.getContentType() ) );
+	@GetMapping("/files")
+	public String getDataset(){
+		return new String(bytes, StandardCharsets.UTF_8);
 	}
+
+
+    @PostMapping("/files")
+    ResponseEntity<ResponseDTO> saveFile(@RequestParam("file") MultipartFile dataFile) {
+
+        try {
+//            InputStream inputStream = dataFile.getInputStream();
+//            int data = inputStream.read();
+//            System.out.println(data);
+			bytes = dataFile.getBytes();
+			ByteArrayInputStream inputFilestream = new ByteArrayInputStream(bytes);
+			BufferedReader br = new BufferedReader(new InputStreamReader(inputFilestream ));
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok(new ResponseDTO<>(Boolean.FALSE, "Name : " + dataFile.getName() + ",Size :" + dataFile.getSize() + ", ContentType : " + dataFile.getContentType()));
+    }
 }
