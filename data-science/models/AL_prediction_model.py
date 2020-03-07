a1db92bbff1d06ea43a7889404aba8ab603b2f6d
randhi-w <randikadinusha@gmail.com>
2020-03-07 11:02:51 +0530

    import statements for test model

---
+++ data-science/models/AL_prediction_model.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+from scipy import stats
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
