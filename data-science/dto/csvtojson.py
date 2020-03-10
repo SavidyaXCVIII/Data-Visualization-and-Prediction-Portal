e404e5a7b611c017668ea2ffbcea6586fc46a467
damithshehan <mdking214@gmail.com>
2020-03-10 14:37:23 +0530

    csv to json try 2

--- data-science/dto/csvtojson.py
+++ data-science/dto/csvtojson.py
@@ -4,7 +4,6 @@ import json
 '''csvfile = open('file.csv', 'r')
 jsonfile = open('file.json', 'w')
 
-fieldnames = ("FirstName","LastName","IDNumber","Message")
 reader = csv.DictReader( csvfile, fieldnames)
 for row in reader:
     json.dump(row, jsonfile)
