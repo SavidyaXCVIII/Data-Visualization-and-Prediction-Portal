e2b65871add0bb9a5350178644681b3dcd4cccdf
Savidya Udesh <45062811+SavidyaXCVIII@users.noreply.github.com>
2020-03-17 15:00:32 +0530

    Added a progress spinner to the education category page.

--- frontend/src/app/modules/home/category/education/education.component.ts
+++ frontend/src/app/modules/home/category/education/education.component.ts
@@ -11,6 +11,7 @@ export class EducationComponent implements OnInit {
 
   readonly ROOT_URL = 'http://localhost:8080';
   dataSets: any;
+  showSpinner = true;
 
 
 getDataSets() {
@@ -23,6 +24,7 @@ getDataSets() {
 
 this.getDataSets().subscribe( response => {
   this.dataSets = response;
+  this.showSpinner = false;
 });
 
   }
