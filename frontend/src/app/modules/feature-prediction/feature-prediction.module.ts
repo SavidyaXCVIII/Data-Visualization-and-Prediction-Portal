83d806f2f801d31897aefa9e7ba54c841792c5d6
randhi-w <randikadinusha@gmail.com>
2020-03-19 10:12:09 +0530

    prediction component added and testing

--- frontend/src/app/modules/feature-prediction/feature-prediction.module.ts
+++ frontend/src/app/modules/feature-prediction/feature-prediction.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeaturePredictionRoutingModule } from './feature-prediction-routing.module';
+import { PredictionComponent } from './prediction/prediction.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PredictionComponent],
   imports: [
     CommonModule,
     FeaturePredictionRoutingModule
