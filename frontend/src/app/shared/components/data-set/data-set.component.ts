ade36ae53c04f17184a32c1abdb1c6b985e50ccb
Savidya Udesh <45062811+SavidyaXCVIII@users.noreply.github.com>
2020-04-24 02:28:02 +0530

    Conversion and retrieval of an array of objects with unknown properties coming from a http response to an array of key value elements and mapping it to the data source of table.

--- frontend/src/app/shared/components/data-set/data-set.component.ts
+++ frontend/src/app/shared/components/data-set/data-set.component.ts
@@ -12,11 +12,38 @@ export class DataSetComponent implements OnInit {
   }
 
   readonly ROOT_URL = 'http://localhost:8080';
-  dataset: [];
+  data: any;
+  dataset: any[];
+  mappedArray: any[] = [];
+  columnHeaders: string[];
 
   ngOnInit() {
     this.getData().subscribe(response => {
-      this.dataset = response;
+      this.data = response;
+    });
+    this.getData().subscribe(response => {
+      this.dataset = Array.of(response);
+      console.log('Original Dataset', this.dataset[0]);
+
+      let count = 1;
+      this.dataset[0].forEach( x => {
+
+        let keyValueArray = [];
+        keyValueArray = Object.keys(x).map( key => [String(key), x[key]]);
+        console.log('keyValueArray ' + count, keyValueArray);
+        this.mappedArray.push(keyValueArray);
+        ++count;
+      });
+      console.log('mapped Array: item 1', this.mappedArray[0]);
+
+      console.log('Items in Mapped Array item 01');
+      this.mappedArray[0].forEach( x => console.log( x[0] + ' : ' + x[1]));
+
+      this.columnHeaders = this.mappedArray[0].map( x => x[0]);
+      this.columnHeaders = this.columnHeaders.filter( x => x !== '_id');
+
+      console.log('Column Headers', this.columnHeaders);
+
     });
   }
 
