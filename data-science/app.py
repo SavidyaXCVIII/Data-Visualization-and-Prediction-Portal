8134e8015ba3faad51d34cc78e24f3c98d4b8094
Savidya Udesh <45062811+SavidyaXCVIII@users.noreply.github.com>
2020-04-23 20:11:22 +0530

    added a response for post api /select_features_predictors

--- data-science/app.py
+++ data-science/app.py
@@ -1,24 +1,29 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from mongodb_connection import MongoDBConnection
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/')
 def hello_world():
     # mongodb_connection = MongoDBConnection()
     # mongodb_connection.insert_record()
-    return 'Hello World! my name is randikaaa '
+    return 'Hello World!'
 
 
 @app.route('/select_features_predictors', methods=['POST'])
+@cross_origin()
 def select():
     data = request.get_json()
     dataset_name = data['dataset_name']
     algorithm = data['algorithm']
     prediction_column = data['prediction_column']
     column_list = data['column_list']
-    return jsonify({'result': 'success', 'dataset_name': dataset_name})
+    resp = jsonify(data)
+    resp.status_code = 200
+    return resp
 
 
 if __name__ == '__main__':
