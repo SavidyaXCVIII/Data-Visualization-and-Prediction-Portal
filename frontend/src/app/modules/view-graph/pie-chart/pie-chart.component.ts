de9319cf73a8d56f109ee83edc177d09ea24010f
isuruAnjula <59241244+isuruAnjula@users.noreply.github.com>
2020-04-26 23:14:25 +0530

    update pie-chart.component.ts
    src\app\modules\view-graph\pie-chart\pie-chart.component.ts

--- frontend/src/app/modules/view-graph/pie-chart/pie-chart.component.ts
+++ frontend/src/app/modules/view-graph/pie-chart/pie-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import * as CanvasJS from '../assets/canvasjs.min';
+import * as CanvasJS from 'src/assets/canvasjs.min';
 import { Observable } from 'rxjs';
 // import { DataModel } from 'src/app/data/data.model';
 import { HttpClient } from '@angular/common/http';
@@ -12,9 +12,67 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PieChartComponent implements OnInit {
 
-  constructor() { }
+  title = 'pie-chart';
 
-  ngOnInit() {
+  // cities: Observable<DataModel>;
+  // Ratnapura: string;
+
+  constructor(private http: HttpClient) {
+
+    // this.cities = this.http.get<DataModel>('./assets/data.json');
+
+    // this.cities.subscribe(events => {
+    //   this.Ratnapura = events[0].District;
+    // });
+
+  }
+
+  ngOnInit () {
+    // https://canvasjs.com/angular-charts/
+    let chart = new CanvasJS.Chart("chartContainer", {
+      
+      theme: "light2",
+      // animationEnabled: true,
+      // exportEnabled: true,
+      // title:{
+      //   text: "Monthly Expense"
+      // },
+      data: [{
+          type: "pie",
+          // showInLegend: true,
+          toolTipContent: "<b>{name}</b>: {y} (#percent%)",
+          indexLabel: "{name} - #percent%",
+          dataPoints: [
+          { y: 69.08, name: "Ratnapura" },
+          { y: 66.04, name: "Mannar" },
+          { y: 65.95, name: "Jaffna" },
+          { y: 65.84, name: "Colombo" },
+          { y: 64.89, name: "Monaragala" },
+          { y: 64.15, name: "Matara"},
+          { y: 64.14, name: "Kilinochchi" },
+          { y: 63.96, name: "Kalutara" },
+          { y: 63.94, name: "Hambantota" },
+          { y: 63.91, name: "Matale" },
+          { y: 63.61, name: "Kegalle" },
+          { y: 63.53, name: "Batticaloa" },
+          { y: 63.42, name: "Galle" },
+          { y: 63.39, name: "Nuwara Eliya" },
+          { y: 63.19, name: "Puttalam" },
+          { y: 62.65, name: "Gampaha" },
+          { y: 62.57, name: "Mullativu" },
+          { y: 62.54, name: "Badulla" },
+          { y: 61.4, name: "Ampara" },
+          { y: 61.24, name: "Trincomalee" },
+          { y: 60.93, name: "Kurunegala" },
+          { y: 60.84, name: "Kandy" },
+          { y: 59.77, name: "Anuradhapura" },
+          { y: 59.32, name: "Vavuniya" },
+          { y: 57.99, name: "Polonnaruwa" }
+          ]
+      }]
+      });
+      chart.render();
+
   }
 
 }
