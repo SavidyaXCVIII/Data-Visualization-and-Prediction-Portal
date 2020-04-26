85b773214fbf999e9af15e97801453694f6306ae
damithshehan <mdking214@gmail.com>
2020-04-26 09:53:36 +0530

    sign-up

--- backend_signup/src/main/java/com/smartdiscover/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.smartdiscover.repository;
-
-import com.smartdiscover.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-
-}
