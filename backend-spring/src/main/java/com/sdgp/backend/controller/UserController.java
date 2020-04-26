856d2d3e8dcd65423ab9b9d984175038ded3b9e0
Nadin Kodithuwakku (NK) <nadin.2018430@iit.ac.lk>
2020-04-27 03:23:58 +0530

    Signup Api save data

--- backend-spring/src/main/java/com/sdgp/backend/controller/UserController.java
+++ backend-spring/src/main/java/com/sdgp/backend/controller/UserController.java
@@ -3,10 +3,13 @@ package com.sdgp.backend.controller;
 import com.sdgp.backend.model.User;
 import com.sdgp.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserController {
@@ -20,6 +23,27 @@ public class UserController {
         User user = new User();
         modelAndView.addObject("user",user);
         modelAndView.setViewName("signup");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findUser(user.getEmail());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "There is already a user registered with the username provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("signup");
+        } else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("login");
+
+        }
         return modelAndView;
     }
 
--- backend-spring/src/main/java/com/sdgp/backend/service/UserService.java
+++ backend-spring/src/main/java/com/sdgp/backend/service/UserService.java
@@ -5,16 +5,29 @@ import com.sdgp.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
+
     public User findUser(String email) {
 
         return userRepository.findByEmail(email);
 
+    }
+
+    public void saveUser(User user) {
+        user.setEmail(user.getEmail());
+        user.setPassword(user.getPassword());
+        user.setFullName(user.getFullName());
+        user.setCompanyName(user.getCompanyName());
+        user.setPhone(user.getPhone());
+        userRepository.save(user);
     }
 
 }
