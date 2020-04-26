31103e571879cd1c6c61750ae1a8b7b5efab1ac6
randhi-w <randikadinusha@gmail.com>
2020-04-26 19:38:02 +0530

    added random forrest algorithm and related api

--- data-science/random_forrest.py
+++ data-science/random_forrest.py
@@ -1,13 +1,28 @@
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-
-
+import pandas as pd
+import numpy as np
+import joblib
 
 def train_random_forrest_model(X, Y):
-
-    regressor = RandomForestRegressor(n_estimators=20, random_state=0)
-    regressor.fit(X, Y)
-    y_pred = regressor.predict(X)
+    df = pd.read_csv("train.csv")
+    rf = RandomForestClassifier(n_estimators=100, random_state=0)
+    X = df[X].values
+    X = np.nan_to_num(X)
+    Y = df[Y].values
+    rf.fit(X, Y)
+    y_pred = rf.predict(X)
     print(confusion_matrix(Y, y_pred))
-    print(classification_report(Y, y_pred))
     print(accuracy_score(Y, y_pred))
+    results_array = {
+        "score": accuracy_score(Y, y_pred),
+
+    }
+    return results_array
+
+def rf_model_prediction(x):
+    filename = "model.sav"
+    load_model = joblib.load(filename)
+    result = load_model.predict(x)
+    print(result)
+    return result
