f048b1172b06e2b89d610515c7711733670bf184
isuruAnjula <59241244+isuruAnjula@users.noreply.github.com>
2020-04-26 23:09:27 +0530

    import to pie-chart.component.ts
    src\app\modules\view-graph\pie-chart\pie-chart.component.ts

--- frontend/src/app/modules/view-graph/pie-chart/pie-chart.component.ts
+++ frontend/src/app/modules/view-graph/pie-chart/pie-chart.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+// import * as CanvasJS from '../assets/canvasjs.min';
+import { Observable } from 'rxjs';
+// import { DataModel } from 'src/app/data/data.model';
+import { HttpClient } from '@angular/common/http';
+// import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-pie-chart',
