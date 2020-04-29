dcbd203cda2e4acfd5278e80ee9b1bf6e63ba753
Savidya Udesh <45062811+SavidyaXCVIII@users.noreply.github.com>
2020-04-29 23:47:58 +0530

    added pie chart

--- frontend/src/app/modules/view-graph/pie-chart/pie-chart.component.ts
+++ frontend/src/app/modules/view-graph/pie-chart/pie-chart.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pie-chart.component.css']
 })
 export class PieChartComponent implements OnInit {
+  public pieChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+  public pieChartData = [120, 150, 180, 90];
+  public pieChartType = 'pie';
+
+  public barChartOptions = {
+    responsive: true,
+  };
+
+  public barChartLegend = true;
 
   constructor() { }
 
