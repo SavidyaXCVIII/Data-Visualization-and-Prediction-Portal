3aa73189925f8b845cfae99b6c5d18b51c23604e
isuruAnjula <59241244+isuruAnjula@users.noreply.github.com>
2020-05-02 00:56:38 +0530

    save csv file from mongodb database

--- data-science/app.py
+++ data-science/app.py
@@ -1,13 +1,21 @@
+import csv
+import urllib
+import urllib.parse
+import pandas
 
+from blaze.compute.tests.test_mongo_compute import pymongo
 from flask import Flask,request, jsonify
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import ast
+
+#from partd import pandas
+
 from mongodb_connection import MongoDBConnection
 app = Flask(__name__)
 
-import pandas as pd
+#import pandas as pd
 import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
@@ -16,6 +24,9 @@ from sklearn import metrics
 import statsmodels.api as sm
 from sklearn import svm
 from sklearn.preprocessing import StandardScaler
+
+from flask import Flask, request, url_for
+from flask_pymongo import PyMongo
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -46,7 +57,7 @@ def hello_world():
 def testcsv():
     mongodb_connection = MongoDBConnection()
 
-    # cursor = mongodb_connection.testData.find({})
+    cursor = mongodb_connection.testData.find({})
 
     return 'Hello World! my name is Isuru! '
 
@@ -54,17 +65,63 @@ def testcsv():
     # mongodb_connection.insert_record()
     return 'Hello World!'
 
+@app.route('/writefile/<dbName>')
+def writeFile(dbName) :
+    print(dbName)
+
+    client = pymongo.MongoClient("mongodb+srv://user_91:sdgp@cluster0-zjhs0.mongodb.net/test?retryWrites=true&w=majority")
+    db = client.test
+
+    print("executing writeFile >>>")
+
+    dbname = dbName
+    cursor = db[dbname].find()
+
+    # extract the list of documents from cursor obj
+    mongo_docs = list(cursor)
+
+    # restrict the number of docs to export
+    mongo_docs = mongo_docs[:200]  # read first 200 lines
+    print("total data:", len(mongo_docs))
+
+    # create an empty DataFrame for storing documents
+    docs = pandas.DataFrame(columns=[]) #create DataFrame named docs
+
+    # iterate over the list of MongoDB dict documents
+    for num, doc in enumerate(mongo_docs):
+        doc["_id"] = str(doc["_id"])
+
+        doc_id = doc["_id"]
+
+        series_obj = pandas.Series(doc, name=doc_id)
+
+        docs = docs.append(series_obj) #insert data
+
+    file_name = dbname + "_file.csv"
+    docs.to_csv(file_name, ",")  # CSV delimited by commas
+
+    df = pandas.read_csv(file_name) #read csv file
+    first_column = df.columns[0]
+    df = df.drop([first_column], axis=1) #remove first column of csv file(unwanted column)
+    df.to_csv(file_name, index=False) #write updated file again to csv
+
+    # export MongoDB documents to CSV
+    # csv_export = docs.to_csv(sep=",")  # CSV delimited by commas
+    # print("\nCSV data:", csv_export) #print csv file in run
+
+    return 'Done!'
+
 @app.route('/select_features_predictors', methods=['GET'])
 @cross_origin()
 def select():
     dataset_id = request.args.get('dataset_id')
+
+    writeFile(dataset_id) # pass collection name to writeFile
+
     dataset_name = request.args.get('dataset_name')
     algorithm = request.args.get('algorithm')
     prediction_column = request.args.get("prediction_column")
     column_list = request.args.get("column_list")
-
-
-
 
     new_column_list = column_list.split(",")
     new_prediction_column = prediction_column.split(",")
