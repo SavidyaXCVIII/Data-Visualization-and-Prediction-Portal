e31ebe948edd8b19ff9684909ef17cff9a0e7b46
Nadin Kodithuwakku (NK) <nadin.2018430@iit.ac.lk>
2020-05-03 13:12:11 +0530

    Signup completed

--- backend-spring/src/main/java/com/sdgp/backend/controller/UserController.java
+++ backend-spring/src/main/java/com/sdgp/backend/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
--- frontend/src/app/services/global.service.ts
+++ frontend/src/app/services/global.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Dataset} from '../models/dataset';
+import {User} from "../models/user";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ export class GlobalService {
   constructor() {}
 
   dataset = new Dataset();
+  user = new User();
 
   getSampleData() {
     return this.dataset;
